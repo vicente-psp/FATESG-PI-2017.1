753fb1d5c27837cbc1448b53615cdfefff3d5036
Alexandre Fernandes <Alexandre Fernande>
2017-06-12 21:47:20 -0300

    Commit de MarcaDAO e ModeloDAO

--- br.com.FATESG.PI-2017.1/src/persistencia/MarcaDAO.java
+++ br.com.FATESG.PI-2017.1/src/persistencia/MarcaDAO.java
@@ -5,33 +5,139 @@
  */
 package persistencia;
 
+import classesdedados.GerarId;
+import classesdedados.Marca;
+import classesdedados.Mensagens;
 import interfacedeclasses.CRUD;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.util.ArrayList;
+import javax.swing.JOptionPane;
 
 /**
  *
  * @author Vicente
  */
 public class MarcaDAO implements CRUD{
-
+    
+    String diretorio = "C:\\Users\\Vicente\\Google Drive\\ADS\\01 - FATESG\\2017.1\\Projetos Equipe ADS\\Projeto Integrador\\br.com.FATESG.PI-2017.1\\br.com.FATESG.PI-2017.1\\FATESG-PI-2017.1\\br.com.FATESG.PI-2017.1\\src\\arquivos\\";
+    String arqMArcas = diretorio + "Marcas.csv";
+        
+    FileWriter fwMarcas = null;
+    BufferedWriter bwMarcas = null;
+    
     @Override
     public void incluir(Object objeto) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+       Marca marca = (Marca) objeto;
+       
+        try {
+            fwMarcas = new FileWriter(arqMArcas, true);
+            bwMarcas = new BufferedWriter(fwMarcas);
+            GerarId gerarId = new GerarId();
+            marca.setIdMarca(gerarId.getIdMarca());
+            String dados = marca.getIdMarca() + ";" + marca.getDescricao() + "/n";
+            bwMarcas.write(dados);
+            bwMarcas.close();
+            
+            gerarId.finalize();
+            JOptionPane.showMessageDialog(null, new Mensagens().mensagem("MSG01"));
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, new Mensagens().mensagem("MSG02"));
+        }
     }
 
     @Override
     public ArrayList<Object> recuperar() throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        ArrayList<Marca> marcas = new ArrayList<>();
+        
+        try {
+            File fileMArcas = new File(arqMArcas);
+            FileReader frMarcas = new FileReader(fileMArcas);
+            BufferedReader brMarcas = new BufferedReader(frMarcas);
+            String linhaMarcas = brMarcas.readLine();
+            while (linhaMarcas != null && !linhaMarcas.equals("")){
+                Marca marca = new Marca();
+                
+                String vMarcas[] = linhaMarcas.split(";");
+                marca.setIdMarca(Integer.parseInt(vMarcas[0]));
+                marca.setDescricao(vMarcas[1]);
+            
+                marcas.add(marca);
+                
+                linhaMarcas = brMarcas.readLine();
+            }            
+        
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, new Mensagens().mensagem("MSG15"));
+        }
+        return (ArrayList<Object>) (Object) (marcas);
     }
 
     @Override
     public void excluir(int id) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Object objMarcas = recuperar();
+        ArrayList<Marca> marcas = (ArrayList<Marca>) (objMarcas);
+        
+        boolean achou = false;
+        String dadosMarca = "";
+        
+        if(marcas.size() >= 0 && marcas != null){
+            for (int i = 0; i < marcas.size(); i++){
+                if(marcas.get(i).getIdMarca() != id){
+                    
+                    dadosMarca += marcas.get(i).getIdMarca() + ";" + marcas.get(i).getDescricao() + "/n";
+                }
+            }
+        }else {
+            achou = true;
+        }
+        if (achou){
+            fwMarcas = new FileWriter(arqMArcas);
+            bwMarcas = new BufferedWriter(fwMarcas);
+            bwMarcas.write(dadosMarca);
+            bwMarcas.close();
+        }
     }
 
     @Override
     public void alterar(int id, Object objeto) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        try {
+            Object objMarcas = recuperar();
+            ArrayList<Marca> marcas = (ArrayList<Marca>) (objMarcas);
+            Marca objMarca = (Marca) objeto;
+            
+            String dadosMarca = "";
+            
+            if(marcas.size() >= 0 && marcas != null){
+                for (int i = 0; i < marcas.size(); i++){
+                    if(marcas.get(i).getIdMarca() != id){
+                        
+                        dadosMarca += marcas.get(i).getIdMarca() + ";" + marcas.get(i).getDescricao() + "/n";
+                    }else {
+                        dadosMarca += id + ";" + objMarca.getDescricao() + "/n";
+                    }
+                }
+                fwMarcas = new FileWriter(arqMArcas);
+                bwMarcas = new BufferedWriter(fwMarcas);
+                bwMarcas.write(dadosMarca);
+                bwMarcas.close();
+                
+                if(!dadosMarca.equals("")){
+                    File file = new File(arqMArcas);
+                    if(!file.exists()){
+                        fwMarcas = new FileWriter(arqMArcas, false);
+                    }else {
+                        fwMarcas = new FileWriter(arqMArcas);
+                    }
+                }
+                JOptionPane.showMessageDialog(null, new Mensagens().mensagem("MSG12"));
+            }
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, new Mensagens().mensagem("MSG13"));
+        }
     }
     
 }
--- br.com.FATESG.PI-2017.1/src/persistencia/ModeloDAO.java
+++ br.com.FATESG.PI-2017.1/src/persistencia/ModeloDAO.java
@@ -6,17 +6,47 @@
 package persistencia;
 
 import interfacedeclasses.CRUD;
+import classesdedados.Modelo;
+import classesdedados.GerarId;
+import classesdedados.Mensagens;
+import java.io.BufferedWriter;
 import java.util.ArrayList;
+import javax.swing.JOptionPane;
+
 
 /**
  *
  * @author Vicente
  */
 public class ModeloDAO implements CRUD{
+    
+    String diretorio = "C:\\Users\\Vicente\\Google Drive\\ADS\\01 - FATESG\\2017.1\\Projetos Equipe ADS\\Projeto Integrador\\br.com.FATESG.PI-2017.1\\br.com.FATESG.PI-2017.1\\FATESG-PI-2017.1\\br.com.FATESG.PI-2017.1\\src\\arquivos\\";
+    String arqModelos = diretorio + "Modelos.csv";
+        
+    FileWriter fwModelos = null;
+    BufferedWriter bwModelos = null;
 
     @Override
     public void incluir(Object objeto) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Modelo modelo = (Modelo) objeto;
+        
+        try {
+            fwModelos = new FileWriter(arqModelos, true);
+            bwModelos = new BufferedWriter(fwModelos);
+            GerarId gerarId = new GerarId();
+            modelo.setIdModelo(gerarId.getIdModelo());
+            String dados = modelo.getIdModelo() + ";" + modelo.getDescricao() + ";" + modelo.getAnoDeFabricacao() + ";" +
+                    modelo.getMarca() + ";" + modelo.getMotor() + ";" + modelo.getValorLocacao() + "/n";
+            bwModelos.write(dados);
+            bwModelos.close();
+            
+            gerarId.finalize();
+            
+            JOptionPane.showMessageDialog(null, new Mensagens().mensagem("MSG01"));
+            
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, new Mensagens().mensagem("MSG02"));
+        }
     }
 
     @Override
